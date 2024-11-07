647f7cbab3c45a28b92299b9ed698b2191c823f9
cynnnia <cynthialorenzolopez@gmail.com>
2024-11-07 17:12:41 +0100

    definitivo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer')
 const mongoose = require('mongoose')
+const fs = require('fs')
 
 require('dotenv').config()
 
@@ -37,47 +38,71 @@ const scrapeProduct = async () => {
   })
 
   const page = await browser.newPage()
-
   await page.goto(url)
 
+  // Quitar modales (si aparecen)
+  try {
+    await page.waitForSelector('.modal-selector', { timeout: 5000 }) // Ajusta el selector del modal si es necesario
+    await page.click('.modal-close-button') // Ajusta el selector del botón de cierre
+    console.log('Modal cerrado')
+  } catch (error) {
+    console.log('No se encontró modal, continuando...')
+  }
+
   await page.type('#js-site-search-input', 'furby')
-
   await page.click('.input-group-btn')
-
   await page.waitForSelector('.glyphicon-chevron-right')
 
-  const title = await page.$$eval('a.name.js-analytics-productClick', (nodes) =>
-    nodes.map((n) => n.innerText)
-  )
+  let allProducts = []
+  let hasNextPage = true
 
-  const price = await page.$$eval('.price', (nodes) =>
-    nodes.map((n) => n.innerText)
-  )
-  const image = await page.$$eval(
-    '.product__listing.product__grid .thumb img',
-    (imgs) => imgs.map((img) => img.src)
-  )
+  while (hasNextPage) {
+    const title = await page.$$eval(
+      'a.name.js-analytics-productClick',
+      (nodes) => nodes.map((n) => n.innerText)
+    )
+    const price = await page.$$eval('.price', (nodes) =>
+      nodes.map((n) => n.innerText)
+    )
+    const image = await page.$$eval(
+      '.product__listing.product__grid .thumb img',
+      (imgs) => imgs.map((img) => img.src)
+    )
 
-  const products = title.slice(0, 25).map((title, index) => {
-    return {
+    const products = title.map((title, index) => ({
       title,
       price: price[index],
       image: image[index]
-    }
-  })
+    }))
 
-  products.map(async (data) => {
-    const dataSchema = new Data(data)
+    allProducts = allProducts.concat(products)
+
+    const nextButton = await page.$(
+      'li.pagination-next a.glyphicon-chevron-right'
+    )
+    if (nextButton) {
+      await nextButton.click()
+      await page.waitForTimeout(2000) // Espera para cargar la siguiente página
+    } else {
+      hasNextPage = false
+    }
+  }
+
+  // Guardar los datos en la base de datos
+  for (let product of allProducts) {
+    const dataSchema = new Data(product)
     try {
       await dataSchema.save()
-      console.log('Data saved')
+      console.log(`Data saved: ${product.title}`)
     } catch (error) {
       console.error(error)
     }
-  })
+  }
+
+  fs.writeFileSync('products.json', JSON.stringify(allProducts, null, 2))
+  console.log('All products saved to products.json')
 
   await browser.close()
-  console.log('all saved Successfully')
 }
 
 scrapeProduct()
